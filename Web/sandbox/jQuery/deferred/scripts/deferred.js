8ab5834b128a99b359eeee0a1578813e00642a5f
mbyrne77 <mbyrne77@comcast.net>
2015-08-12 22:08:44 -0400

    Added jQuery deffered content to sandbox

---
+++ Web/sandbox/jQuery/deferred/scripts/deferred.js
@@ -0,0 +1,6 @@
+﻿/// <reference path="../../jquery-2.1.4.js" />
+var sandbox = sandbox || {};
+sandbox.app = sandbox.app || {};
+sandbox.app.jQuery = sandbox.app.jQuery || {};
+sandbox.app.jQuery.deferred = sandbox.app.jQuery.deferred || {};
+
